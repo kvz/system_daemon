ee3c71e8849520a91abc3b957918832abac36fcf
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-04-22 21:04:20 +0000

    more packaging tryouts

--- example_logparser.php
+++ example_logparser.php
@@ -37,6 +37,7 @@ $runmode = array(
     "write-initd" => false
 );
 
+// scan ar
 foreach ($argv as $k=>$arg) {
     if (substr($arg, 0, 2) == "--" && isset($runmode[substr($arg, 2)])) {
         $runmode[substr($arg, 2)] = true;
--- package2.php
+++ package2.php
@@ -1,18 +1,15 @@
 <?php
-/*
- * Limb PHP Framework
+/**
+ * Script to generate package.xml file
  *
- * @link http://limb-project.com
- * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html
+ * Taken from Limb PHP Framework http://limb-project.com 
+ * More info http://www.developertutorials.com/pear-manual/developers.packagedef.html
+ *
+ * @version   SVN: Release: $Id$
  */
 
-/**
- * @package tree
- * @version $Id$
- */
-require_once 'PEAR/PackageFileManager.php';
-require_once 'PEAR/PackageFileManager/Svn.php';
+require_once 'PEAR/PackageFileManager2.php';
+//require_once 'PEAR/Svn.php';
 
 list($name, $baseVersion, $state) = explode('-', trim(file_get_contents(dirname(__FILE__) . '/docs/VERSION')));
 $changelog = htmlspecialchars(file_get_contents(dirname(__FILE__) . '/docs/CHANGELOG'));
@@ -29,59 +26,57 @@ $apiStability = $state;
 $package = new PEAR_PackageFileManager2();
 
 $result = $package->setOptions(array(
-    'license'           => 'LGPL',
+    'package'           => $name,
+    'summary'           => $summary,
+    'version'           => $version,
+    'state'             => $state,
+    'description'       => $description,
     'filelistgenerator' => 'file',
     'ignore'            => array('package.php',
                                  'package.xml',
                                  '*.tgz',
                                  'docs'),
     //'simpleoutput'      => true,
+    'baseinstalldir'  => 'System',
     'packagedirectory'  => './',
     'packagefile' => 'package.xml',
     'dir_roles' => array('docs' => 'doc',
                          'examples' => 'doc',
                          'tests' => 'test'),
     'roles' => array('*' => 'php'),
-    ));
-if(PEAR::isError($result))
-{
-  echo $result->getMessage();
-  exit(1);
+));
+    
+if (PEAR::isError($result)) {
+    echo $result->getMessage();
+    exit(1);
 }
 
-$package->setPackage($name);
-$package->setSummary($summary);
-$package->setDescription($description);
-
-$package->setChannel('pear.limb-project.com');
 $package->setAPIVersion($apiVersion);
 $package->setReleaseVersion($version);
 $package->setReleaseStability($state);
 $package->setAPIStability($apiStability);
 $package->setNotes($changelog);
 $package->setPackageType('php');
-$package->setLicense('New BSD Licence', 'http://www.opensource.org/licenses/bsd-license.php ');
+$package->setLicense('BSD', 'http://www.opensource.org/licenses/bsd-license.php');
 
-foreach($maintainers as $line)
-{
-  list($role, $nick, $name, $email, $active) = explode(',', $line);
-  $package->addMaintainer($role, $nick, $name, $email, $active);
+foreach ($maintainers as $line) {
+    $parts = explode(',', $line);
+    list($role, $nick, $name, $email, $active) = $parts;
+    //print_r($parts);
+    $package->addMaintainer($role, $nick, $name, $email);
 }
-
-$package->setPhpDep('5.1.4');
-$package->setPearinstallerDep('1.4.99');
-
-$package->addPackageDepWithChannel('required', 'core', 'pear.limb-project.com', '0.2.0');
-$package->addPackageDepWithChannel('required', 'dbal', 'pear.limb-project.com', '0.1.0');
-
 $package->generateContents();
+
+/*$package->addDependency('php',              '5.2.1', 'ge',  'php', false);
+$package->addDependency('PEAR',             '1.3.3', 'ge',  'pkg', false);
+$package->addDependency('Linux',            false,   'has', 'os',  false);
+*/
 
 $result = $package->writePackageFile();
 
-if(PEAR::isError($result))
-{
-  echo $result->getMessage();
-  exit(1);
+if (PEAR::isError($result)) {
+    echo $result->getMessage();
+    exit(1);
 }
 
 ?>
