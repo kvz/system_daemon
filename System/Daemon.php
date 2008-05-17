cbf3864220512e78f44b8385e919722a40c53105
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-05-17 14:18:12 +0000

    Changed author

--- System/Daemon.php
+++ System/Daemon.php
@@ -8,7 +8,7 @@
  *
  * @category  System
  * @package   System_Daemon
- * @author    Kevin <kevin@vanzonneveld.net>
+ * @author    Kevin van Zonneveld <kevin@vanzonneveld.net>
  * @copyright 2008 Kevin van Zonneveld (http://kevin.vanzonneveld.net)
  * @license   http://www.opensource.org/licenses/bsd-license.php New BSD Licence
  * @version   SVN: Release: $Id$
@@ -29,7 +29,7 @@ spl_autoload_register(array("System_Daemon", "autoload"));
  *
  * @category  System
  * @package   System_Daemon
- * @author    Kevin <kevin@vanzonneveld.net>
+ * @author    Kevin van Zonneveld <kevin@vanzonneveld.net>
  * @copyright 2008 Kevin van Zonneveld (http://kevin.vanzonneveld.net)
  * @license   http://www.opensource.org/licenses/bsd-license.php New BSD Licence
  * @version   SVN: Release: $Id$
--- test.php
+++ test.php
@@ -8,7 +8,7 @@
  * 
  * @category  System
  * @package   System_Daemon
- * @author    Kevin <kevin@vanzonneveld.net>
+ * @author    Kevin van Zonneveld <kevin@vanzonneveld.net>
  * @copyright 2008 Kevin van Zonneveld (http://kevin.vanzonneveld.net)
  * @license   http://www.opensource.org/licenses/bsd-license.php New BSD Licence
  * @version   SVN: Release: $Id$
