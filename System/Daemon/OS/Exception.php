3f41a4804c66cc9d242f712090c6683a1ec230cf
Kevin van Zonneveld <kevin@vanzonneveld.net>
2008-12-22 13:22:45 +0000

    Readded OSes?! Damn you svn!

---
+++ System/Daemon/OS/Exception.php
@@ -0,0 +1,33 @@
+<?php
+/* vim: set noai expandtab tabstop=4 softtabstop=4 shiftwidth=4: */
+/**
+ * System_Daemon turns PHP-CLI scripts into daemons.
+ * 
+ * PHP version 5
+ *
+ * @category  System
+ * @package   System_Daemon
+ * @author    Kevin van Zonneveld <kevin@vanzonneveld.net>
+ * @copyright 2008 Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD Licence
+ * @version   SVN: Release: $Id$
+ * @link      http://trac.plutonia.nl/projects/system_daemon
+ */
+
+/**
+ * An exception thrown by System_Daemon_OS when it encounters an unrecoverable error.
+ *
+ * @category  System
+ * @package   System_Daemon
+ * @author    Kevin van Zonneveld <kevin@vanzonneveld.net>
+ * @copyright 2008 Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD Licence
+ * @version   SVN: Release: $Id$
+ * @link      http://trac.plutonia.nl/projects/system_daemon
+ * * 
+ */
+class System_Daemon_OS_Exception extends PEAR_Exception
+{
+
+}//end class
+?>
---
+++ System/Daemon/OS/Windows.php
@@ -0,0 +1,45 @@
+<?php
+/* vim: set noai expandtab tabstop=4 softtabstop=4 shiftwidth=4: */
+/**
+ * System_Daemon turns PHP-CLI scripts into daemons.
+ * 
+ * PHP version 5
+ *
+ * @category  System
+ * @package   System_Daemon
+ * @author    Kevin van Zonneveld <kevin@vanzonneveld.net>
+ * @copyright 2008 Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD Licence
+ * @version   SVN: Release: $Id$
+ * @link      http://trac.plutonia.nl/projects/system_daemon
+ */
+
+/**
+ * A System_Daemon_OS driver for Windows
+ *
+ * @category  System
+ * @package   System_Daemon
+ * @author    Kevin van Zonneveld <kevin@vanzonneveld.net>
+ * @copyright 2008 Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD Licence
+ * @version   SVN: Release: $Id$
+ * @link      http://trac.plutonia.nl/projects/system_daemon
+ * * 
+ */
+class System_Daemon_OS_Windows extends System_Daemon_OS
+{
+    /**
+     * Determines wether this system is compatible with this OS
+     *
+     * @return boolean
+     */
+    public function isInstalled() 
+    {
+        if (strtoupper(substr(PHP_OS, 0, 3)) !== "WIN") {
+            return false;
+        }
+        
+        return true;
+    }//end isInstalled
+}//end class
+?>
