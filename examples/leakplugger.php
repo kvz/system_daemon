809262b4e307852cff8507c7da2594e02fd9550a
Kevin van Zonneveld <kevin@vanzonneveld.net>
2010-11-09 12:28:49 +0100

    Coding style

--- examples/leakplugger.php
+++ examples/leakplugger.php
@@ -1,15 +1,16 @@
 <?php
 require_once "../System/Daemon.php";
-System_Daemon::setOption("appName", "memeater");
+System_Daemon::setOption("appName", "leakplugger");
 System_Daemon::start();
 $last = 0;
-while(!System_Daemon::isDying()){
- $mem = memory_get_peak_usage();
- $use = $mem - $last;
- if ($use >= 0) $use = '+' . $use;
- System_Daemon::info("test");
- echo "debug: memory_get_peak_usage: " . $use . "\n";
- #sleep(1);
- System_Daemon::iterate(1);
- $last = $mem;
+while (!System_Daemon::isDying()){
+    $mem = memory_get_peak_usage();
+    $use = $mem - $last;
+    if ($use >= 0) $use = '+' . $use;
+
+    System_Daemon::info("test");
+    echo "debug: memory_get_peak_usage: " . $use . "\n";
+
+    $last = $mem;
+    System_Daemon::iterate(1);
 }
